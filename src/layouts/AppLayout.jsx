74ea494320b6761d2ecc1bfd30b91bac256cd733
Ganesh_Biradar <bganesh2468@gmail.com>
2025-07-26 15:24:53 +0530

    added hover animation on the name at the footer

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -10,7 +10,15 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <footer className="p-10 text-center bg-gray-800 mt-10">
-        <p>Made with 💖 by <a href="https://github.com/ganesh1514" className = "text-primary-orange cursor-pointer hover:underline-primary-orange hover:underline-offset-4">Biradar Ganesh</a></p>
+        <p>
+          Made with 💖 by{" "}
+          <a
+            href="https://github.com/ganesh1514"
+            className="relative text-primary-orange cursor-pointer before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-0 before:h-0.5 before:bg-primary-orange before:transition-all before:duration-300 hover:before:w-full"
+          >
+            Biradar Ganesh
+          </a>
+        </p>
       </footer>
       <Toaster position="top-center" />
     </div>
